4038b0ec0b566095a31c86f9ec5bee4b3cb16d28
Tjabe Johannes van Lessen <tjabejv@stud.ntnu.no>
2019-09-19 17:14:36 +0200

    Updates

---
+++ precache-manifest.ab02eb716bffbc7bcdfe275375db91fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1868e68a9c12c55233a81b55cb59ef",
+    "url": "/TopJSRepo/index.html"
+  },
+  {
+    "revision": "28c98b09c14f033aec9a",
+    "url": "/TopJSRepo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "da77d6d8b2d75101f7dc",
+    "url": "/TopJSRepo/static/js/2.afd9b245.chunk.js"
+  },
+  {
+    "revision": "28c98b09c14f033aec9a",
+    "url": "/TopJSRepo/static/js/main.e39968fd.chunk.js"
+  },
+  {
+    "revision": "92f2c91b8291ab4afdb9",
+    "url": "/TopJSRepo/static/js/runtime~main.9528bbf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TopJSRepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
